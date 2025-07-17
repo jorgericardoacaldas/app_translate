969638f6b5f0a71466435843b3a3aa6d88d4ddbb
jorgericardoacaldas <ricardo10caldas@gmail.com>
2025-07-17 00:22:21 -0300

    editing screen

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -9,34 +9,58 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ColoredBox(
-                color: Colors.blueGrey,
-                child: Row(
-                  children: [
-                    CustomDropdownMenu(languages: [
-                      "English",
-                      "Spanish",
-                      "French",
-                      "German",
-                      "Chinese",
-                      "Japanese",
-                    ], initialSelection: 'English'),
-                    Icon(Icons.arrow_forward, color: Colors.white),
-                    CustomDropdownMenu(languages: [
-                      "English",
-                      "Spanish",
-                      "French",
-                      "German",
-                      "Chinese",
-                      "Japanese",],
-                      initialSelection: 'Spanish'),
-                  ],
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: ColoredBox(
+                    color: Colors.blueGrey,
+                    child: Row(
+                      children: [
+                        CustomDropdownMenu(
+                          languages: [
+                            "English",
+                            "Spanish",
+                            "French",
+                            "German",
+                            "Chinese",
+                            "Japanese",
+                          ],
+                          initialSelection: 'English',
+                        ),
+                        Icon(Icons.arrow_forward, color: Colors.white),
+                        CustomDropdownMenu(
+                          languages: [
+                            "English",
+                            "Spanish",
+                            "French",
+                            "German",
+                            "Chinese",
+                            "Japanese",
+                          ],
+                          initialSelection: 'Spanish',
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ],
+
+                ColoredBox(
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      TextField(maxLines: 6),
+                      Spacer(),
+
+                      Divider(indent: 10, endIndent: 10),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
